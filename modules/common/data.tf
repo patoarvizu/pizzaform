85b5e7a3f944cd4a300728472777ec837c297ad6
patoarvizu <patoarvizu@gmail.com>
2019-09-29 15:54:51 -0400

    Remove full 'menu' object

--- modules/common/data.tf
+++ modules/common/data.tf
@@ -8,7 +8,3 @@ data "dominos_address" "home" {
 data "dominos_store" "store" {
   address_url_object = "${data.dominos_address.home.url_object}"
 }
-
-data "dominos_menu" "menu" {
-  store_id = "${data.dominos_store.store.store_id}"
-}
